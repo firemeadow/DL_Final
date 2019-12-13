637a8593e0be758c3c75fe6f4c5aaad6beee1026
lmi12 <lmi12@case.edu>
2019-12-13 16:22:21 -0500

    nothing significant, added separate files for gen and disc models

--- stock_gan.py
+++ stock_gan.py
@@ -1,19 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib as plt
+from gen import Generator
+from disc import Discriminator
 
 BUFFER_SIZE = 1000
 BATCH_SIZE = 250
 NUM_BATCHES = int(np.ceil(1000/250))
 NUM_EPOCHS = 100
-
-def make_discriminator():
-    model = 0
-    return model
-
-def make_generator():
-    model = 0
-    return model
 
 def discriminator_loss(real_output, fake_output):
     real_loss = real_output
@@ -24,12 +18,12 @@ def discriminator_loss(real_output, fake_output):
 def generator_loss(fake_output):
     return fake_output
 
-def train_step(data, labels, generator, discriminator):
+def train_step(data, labels, gen_model, disc_model):
     noise = np.random.normal()
-    generated_samples = generator(noise)
+    generated_samples = gen_model(noise)
 
-    fake_output = discriminator(generated_samples)
-    real_output = discriminator(data)
+    fake_output = disc_model(generated_samples)
+    real_output = disc_model(data)
 
     gen_loss = generator_loss(fake_output)
     disc_loss = discriminator_loss(real_output, fake_output)
@@ -38,13 +32,13 @@ def train_step(data, labels, generator, discriminator):
 
 
 def train(data, labels):
-    generator = make_generator()
-    discriminator = make_discriminator()
+    gen_model = Generator()
+    disc_model = Discriminator()
     gens = []
     discs = []
     for epoch in range(NUM_EPOCHS):
         for data_batch, label_batch in zip(data, labels):
-            gen_loss, disc_loss = train_step(data_batch, label_batch, generator, discriminator)
+            gen_loss, disc_loss = train_step(data_batch, label_batch, gen_model, disc_model)
 
         gen = gen_loss.numpy().mean()
         disc = disc_loss.numpy().mean()
