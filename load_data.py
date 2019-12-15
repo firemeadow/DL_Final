e8c16215da165ee24c502f925690ea5ac5ae3888
lmi12 <lmi12@case.edu>
2019-12-14 19:41:25 -0500

    cleaned up data loading

--- load_data.py
+++ load_data.py
@@ -4,27 +4,83 @@ import pandas as pd
 import pandas_datareader as web
 import numpy as np
 
-if __name__ == '__main__':
-    ticker_names = np.asarray(pd.read_csv('tickers.txt', delimiter=',', encoding="utf-8-sig", header=None).values.tolist()).flatten()
-    start = dt.datetime(2015, 1, 1)
+
+def fourier_transforms(dataset):
+    data_FT = dataset['adjusted close']
+    close_fft = np.fft.fft(np.asarray(data_FT.tolist()))
+    fft_df = pd.DataFrame({'fft': close_fft})
+    fft_df['absolute'] = fft_df['fft'].apply(lambda x: np.abs(x))
+    fft_df['angle'] = fft_df['fft'].apply(lambda x: np.angle(x))
+    fft_list = np.asarray(fft_df['fft'].tolist())
+    for num_ in [3, 6, 9]:
+        fft_list_m10 = np.copy(fft_list)
+        fft_list_m10[num_:-num_] = 0
+        dataset["fourier " + str(num_)] = np.fft.ifft(fft_list_m10)
+
+    return dataset
+
+
+def get_technical_indicators(dataset):
+    # Create 7 and 21 days Moving Average
+    dataset['ma7'] = dataset['adjusted close'].rolling(window=7).mean()
+    dataset['ma21'] = dataset['adjusted close'].rolling(window=21).mean()
+
+    # Create MACD
+    dataset['26ema'] = dataset['adjusted close'].ewm(span=26).mean()
+    dataset['12ema'] = dataset['adjusted close'].ewm(span=12).mean()
+    dataset['MACD'] = (dataset['12ema'] - dataset['26ema'])
+
+    # Create Bollinger Bands
+    dataset['20sd'] = dataset['adjusted close'].rolling(20).std()
+    dataset['upper_band'] = dataset['ma21'] + (dataset['20sd'] * 2)
+    dataset['lower_band'] = dataset['ma21'] - (dataset['20sd'] * 2)
+
+    # Create Exponential moving average
+    dataset['ema'] = dataset['adjusted close'].ewm(com=0.5).mean()
+
+    # Create Momentum
+    dataset['momentum'] = dataset['adjusted close'] - 1
+
+    return dataset
+
+
+def get_av_data(symbol, type='av-daily-adjusted'):
+    start = dt.datetime(2010, 1, 1)
     end = dt.datetime.now()
-    data = pd.read_csv('data.txt', delimiter=',', encoding='utf-8-sig', header=None)
-    i = 0
-    for name in ticker_names:
-        recieved_data = False
-        print(i)
-        temp_ticker_names = ticker_names[i + 1:]
-        while not recieved_data:
-            try:
-                df = web.DataReader(name, 'av-daily', start, end, api_key='L43U3XRM44TNHHE7')
-                df = df[['open', 'close']].values.flatten()
-                df = pd.DataFrame(df)
-                data.append(df)
-                np.savetxt('data.txt', data, fmt='%s', delimiter=',')
-                np.savetxt('tickers.txt', temp_ticker_names, fmt='%s', delimiter=',')
-                time.sleep(1)
-            except web._utils.RemoteDataError:
-                time.sleep(10)
-            else:
-                recieved_data = True
-        i += 1
+    key = 'L43U3XRM44TNHHE7'
+
+    data_received = False
+    while not data_received:
+        try:
+            data = web.DataReader(symbol, type, start, end, api_key=key)
+            time.sleep(2)
+        except web._utils.RemoteDataError:
+            time.sleep(5)
+        else:
+            data_received = True
+    return data
+
+def load(company1, company2, competitors):
+    c1 = get_av_data(company1)
+    c1 = get_technical_indicators(c1)
+    c1 = fourier_transforms(c1)
+
+    c2 = get_av_data(company2)
+    c2 = get_technical_indicators(c2)
+    c2 = fourier_transforms(c2)
+
+    for i, col in c2.items():
+        new_name = col.name + " B"
+        c1[new_name] = col
+
+    for name in competitors:
+        data = get_av_data(name)
+        c1[name] = data['adjusted close']
+
+    return c1
+
+
+if __name__ == '__main__':
+    competitors = ['AVVIY', 'JEF', 'PGR', 'AIG', 'STFGX', 'BLK']
+    data = load('BRK.A', 'BRK.B', competitors)
+    exit(0)
--- stock_gan.py
+++ stock_gan.py
@@ -6,43 +6,41 @@ import numpy as np
 import matplotlib as plt
 from gen import Generator
 from disc import Discriminator
+from load_data import load
 
-BUFFER_SIZE = 1000
-BATCH_SIZE = 250
-NUM_BATCHES = int(np.ceil(BUFFER_SIZE/BATCH_SIZE))
-NUM_EPOCHS = 100
-pos_weights = torch.ones((BATCH_SIZE,))
-loss = nn.BCEWithLogitsLoss(pos_weights=pos_weights)
-learning_rate = 0.01
 
-def discriminator_loss(real_output, fake_output):
-    real_target = torch.ones(torch.shape(real_output))
-    fake_target = torch.zeros(torch.shape(fake_output))
+def discriminator_loss(real_output, fake_output, loss):
+    real_target = torch.ones(torch.np(real_output))
+    fake_target = torch.zeros(torch.np(fake_output))
     real_loss = loss(real_output, real_target)
     fake_loss = loss(fake_output, fake_target)
     total_loss = real_loss + fake_loss
     return total_loss
 
 
-def generator_loss(fake_output):
-    fake_target = torch.ones(torch.shape(fake_output))
+def generator_loss(fake_output, loss):
+    fake_target = torch.ones(np.shape(fake_output))
     return loss(fake_output, fake_target)
 
 
 def train_step(data, labels, gen_model, disc_model):
     num_attr = len(data.columns)
+    pos_weights = torch.ones((BATCH_SIZE,))
+    loss = nn.BCEWithLogitsLoss(pos_weights=pos_weights)
     noise = np.random.normal(size=(num_attr,))
     generated_samples = gen_model(noise)
 
     fake_output = disc_model(generated_samples)
     real_output = disc_model(data)
 
-    gen_loss = generator_loss(fake_output)
-    disc_loss = discriminator_loss(real_output, fake_output)
+    gen_loss = generator_loss(fake_output, loss)
+    disc_loss = discriminator_loss(real_output, fake_output, loss)
     return gen_loss, disc_loss
 
 
-def train(data, labels):
+def train(data):
+    learning_rate = 0.01
+    NUM_EPOCHS = 100
     num_attr = len(data.columns)
     gen_model = Generator(num_attr)
     gen_opt = optim.SGD(gen_model.parameters(), lr=learning_rate)
@@ -51,10 +49,10 @@ def train(data, labels):
     gens = []
     discs = []
     for epoch in range(NUM_EPOCHS):
-        for data_batch, label_batch in zip(data, labels):
+        for data_batch in data:
             gen_opt.zero_grad()  # zero the gradient buffers
             disc_opt.zero_grad()
-            gen_loss, disc_loss = train_step(data_batch, label_batch, gen_model, disc_model)
+            gen_loss, disc_loss = train_step(data_batch, gen_model, disc_model)
             gen_loss.backward()
             disc_loss.backward()
             gen_opt.step()
@@ -70,9 +68,24 @@ def train(data, labels):
 
 
 if __name__ == '__main__':
-    data = pd.read_csv('data.txt', delimiter=',')
-    labels = data[[-1]]
+    competitors = ['AVVIY', 'JEF', 'PGR', 'AIG', 'STFGX', 'BLK']
+    data = load('BRK.A', 'BRK.B', competitors)
+    num_days = len(data.columns) / 5
+    BATCH_SIZE = 250
+    train_days = int(np.ceil(num_days * (3 / 5)))
+    NUM_BATCHES = int(np.ceil(train_days / BATCH_SIZE))
+    #test_days = num_days - train_days
 
-    gens, discs, generator, discriminator = train(data, labels)
+    ###BATCHING DOES NOT WORK
+
+    raw_train_data = np.reshape(data[:train_days].to_numpy(), (train_days, 5))
+    #raw_test_data = np.reshape(data[train_days + 1:].to_numpy(), (test_days, 5))
+
+    batched_train_data = []
+    for batch in range(NUM_BATCHES):
+        temp = raw_train_data[batch * BATCH_SIZE: (batch + 1) * BATCH_SIZE]
+        batched_train_data[batch] = temp
+
+    gens, discs, generator, discriminator = train(batched_train_data)
 
 
